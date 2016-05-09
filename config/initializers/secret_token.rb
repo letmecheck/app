2a98c0b53ddea48772ddfe40dbd6d4f7901c9272
Elmer <efe_santos@hotmail.com>
2016-05-09 17:58:34 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Letmecheck::Application.config.secret_key_base = 'c9ede34788b33262d1640861646ff7cbf3cea88ad7df5790a5de85efc4def6fb78a15c7cdc0ff0fc5f08e861a6882b47818018521ab18edf56afd4e487fd43f8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Letmecheck::Application.config.session_store :cookie_store, key: '_letmecheck_session'
