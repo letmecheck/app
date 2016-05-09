461ffe0ac4fe04066168268c619fb2f421c60461
Elmer <efe_santos@hotmail.com>
2016-05-09 18:14:58 +0000

    Generate static_page controller, index action and view

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Letmecheck::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+  root 'static_page#index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
