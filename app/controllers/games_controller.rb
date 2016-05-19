c24ee904620be06e0a979e36265f8cf9406b7950
bennypaulino <santiagopro71@gmail.com>
2016-05-19 11:33:00 -0400

    Adjust the index page, adjust games_controller, and rename game folder to games.

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,21 @@
+class GamesController < ApplicationController
+	#before_action :authenticate_user!
+	def new
+		@game = Game.new
+	end
+
+	def create
+		@game = Game.create(game_params)
+		redirect_to game_path(@game)
+	end
+
+	def show
+		@game = Game.find(params[:id])		
+	end
+
+	private
+
+	def game_params
+		params.require(:game).permit(:name, :white_player_id, :black_player_id)
+	end
+end
