99ac6a9691b614ceebf62a02a18bcea83614226d
Elmer <efe_santos@hotmail.com>
2016-05-19 20:40:06 +0000

    Refactor code in games controller create method

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,15 +1,9 @@
 class GamesController < ApplicationController
 	#before_action :authenticate_user!
-	def new
-		@game = Game.new
-	end
 
 	def create
-		@game = Game.new(	name: "test",
-											white_player_id: 1,
-											black_player_id: 1)
-		@game.save 
-		redirect_to game_path(@game)
+		@game = Game.create 
+		redirect_to @game
 	end
 
 	def show
