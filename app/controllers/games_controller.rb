0c88b2dbe3e1d5905cbb0b36f0848161f05f4dfa
Prateek Vasireddy <prat.vasireddy@gmail.com>
2016-05-23 00:58:16 +0000

    Added current_game helper method and added join button to the game index pages

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -10,22 +10,22 @@ class GamesController < ApplicationController
 	end
 
 	def create
-		@game = Game.create(game_params)
+		@game = Game.find(params[:id])
 		@game.white_player_id = current_user.id
 		@game.save
 		redirect_to game_path(@game)
 	end
 
 	def show
-		@game = Game.find(params[:id])
+		@white_player = User.find(current_game.white_player_id)
+		@black_player = User.find(current_game.black_player_id)
 	end
 
 	def update
-		@game = Game.find(params[:id])
-		if @game.black_player_id.nil? && @game.white_player_id != current_user.id
-			@game.black_player_id = current_user.id 
-			@game.save
-			redirect_to game_path(@game)
+		if current_game.black_player_id.nil? && current_game.white_player_id != current_user.id
+			current_game.black_player_id = current_user.id 
+			current_game.save
+			redirect_to game_path(current_game)
 		else
 			render text: 'The game is already full!', status: :unauthorized	
 		end
@@ -35,6 +35,11 @@ class GamesController < ApplicationController
 
 	def game_params
 		params.require(:game).permit(:name)
+	end
+
+	helper_method :current_game	
+	def current_game
+		@current_game ||= Game.find(params[:id])	
 	end
 
 	# def game_params
