30a7dc01c545ffa374fd5d596a46b5128e0116e4
Prateek Vasireddy <prat.vasireddy@gmail.com>
2016-05-28 21:50:14 +0000

    Attempted to debug the helper methods for the game show page

--- app/helpers/game_helper.rb
+++ app/helpers/game_helper.rb
@@ -1,7 +1,7 @@
 module GameHelper
-  def display_pieces(x, y)
+  def display_game_pieces(x, y)
+    @game = Game.find(params[:id])
     @piece = @game.pieces.where(x_coord: x, y_coord: y).first
-    #image_tag(link_to @piece.img, class: "img-responsive") if @piece.present?
     link_to image_tag(@piece.img, class: "img-responsive"), piece_path(@piece) if @piece.present?
   end
 end
