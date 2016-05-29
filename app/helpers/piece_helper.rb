e04729bbdf95427a3525ac82a2d83499b7b7a28d
Prateek Vasireddy <prat.vasireddy@gmail.com>
2016-05-29 21:59:24 +0000

    Styled the move button and highlighted the selected piece

--- app/helpers/piece_helper.rb
+++ app/helpers/piece_helper.rb
@@ -1,7 +1,7 @@
 module PieceHelper
   def display_piece_pieces(x, y)
     if @piece == @piece.game.pieces.where(x_coord: x, y_coord: y).first
-      link_to image_tag(@piece.img, class: "img-responsive active selected-piece"), piece_path(@piece) if @piece.present?
+      image_tag(@piece.img, class: "img-responsive active selected-piece") if @piece.present?
     else
       @other_piece = @piece.game.pieces.where(x_coord: x, y_coord: y).first
       image_tag(@other_piece.img, class: "img-responsive non-selected-piece") if @other_piece.present?
