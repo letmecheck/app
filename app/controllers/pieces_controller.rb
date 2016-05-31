426722af45422dfd6fa6e8b85d04a3a4f6686e42
bennypaulino <santiagopro71@gmail.com>
2016-05-31 14:39:29 -0400

    Move the chess board to a partial

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -7,7 +7,11 @@ class PiecesController < ApplicationController
   def update
     @piece = Piece.find(params[:id])
     @piece.move_to!(piece_params[:x_coord], piece_params[:y_coord])
-    redirect_to game_path(@piece.game)
+    #redirect_to game_path(@piece.game)
+    respond_to do |format|
+      format.html { redirect_to game_path(@piece.game) }
+      format.js
+    end
   end
 
   private
--- app/helpers/game_helper.rb
+++ app/helpers/game_helper.rb
@@ -1,6 +1,5 @@
 module GameHelper
-  def display_game_pieces(x, y)
-    @piece = @game.pieces.find_by(x_coord: x, y_coord: y)
-    # link_to image_tag(@piece.img, class: "img-responsive"), piece_path(@piece) if @piece.present?
-  end
+#   def display_game_pieces(x, y)
+#     @piece = @game.pieces.find_by(x_coord: x, y_coord: y)
+#   end
 end
