a742ccfd0f162ac10d5935152d45174781556d32
Elmer <efe_santos@hotmail.com>
2016-05-31 14:42:47 +0000

    Delete commented out line on games helper

--- app/helpers/game_helper.rb
+++ app/helpers/game_helper.rb
@@ -2,7 +2,6 @@ module GameHelper
   def display_game_pieces(x, y)
     @game = Game.find(params[:id])
     @piece = @game.pieces.where(x_coord: x, y_coord: y).first
-    #link_to image_tag(@piece.img, class: "img-responsive"), piece_path(@piece) if @piece.present?
     image_tag(@piece.img, class: "img-responsive draggable") if @piece.present?
   end
 end
