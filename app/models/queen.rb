cbd6b4316aeaaae0ebc5ca9e7c857dbdd3d7ec25
Elmer <efe_santos@hotmail.com>
2016-05-31 16:56:07 +0000

    Call obstructed? method in the queen's valid_move? method

--- app/models/queen.rb
+++ app/models/queen.rb
@@ -4,6 +4,7 @@ class Queen < Piece
 
     return false if off_board?(new_x, new_y)
     return false if current_square?(new_x, new_y)
+    return false if obstructed?(new_x, new_y)
     return true if linear_move?(new_x, new_y)
 
     false
