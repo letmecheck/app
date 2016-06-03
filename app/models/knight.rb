80c14c2c100e84121c1bd1a3d0d9959a31bc5b35
bennypaulino <santiagopro71@gmail.com>
2016-06-03 19:54:23 -0400

    Refactor valid_move? method for the knight

--- app/models/knight.rb
+++ app/models/knight.rb
@@ -1,13 +1,8 @@
 class Knight < Piece
     def valid_move?(new_x, new_y)
         return false if off_board?(new_x, new_y)
-        return false if current_square?(new_x, new_y)
-    
+
         x_offset, y_offset = movement_by_axis(new_x, new_y)
- 
-        return true if x_offset.abs == 2 && y_offset.abs == 1 
-        return true if x_offset.abs == 1 && y_offset.abs == 2
-        
-        false        
-    end    
+        x_offset.abs == 2 && y_offset.abs == 1 || x_offset.abs == 1 && y_offset.abs == 2
+    end
 end
