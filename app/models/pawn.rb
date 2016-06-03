cc8c6107d5a1d4708fbe2b21aaf4c664afb4f414
Steve McClellan <steve470@gmail.com>
2016-06-03 16:17:28 -0400

    Add pawn-specific move_to! method
    Create a move_to! method in the Pawn model which overrides (and then
    calls, via "super") the Piece model's move_to! method.
    
    The Pawn-specific variety destroys a pawn that has been captured en
    passant (via the capture_en_passant! helper method), and updates the
    game's en_passant_file attribute with the file number (x_coord) if the
    current move is a two-square advance (thus leaving the currently-moving
    pawn vulnerable to en passant capture on the next move), and nil
    otherwise.
    
    After the call to super, which handles standard captures and updates the
    coordinates of the moving piece, a commented-out call to the
    not-yet-created promote! method is added as a placeholder.
    
    The capture_en_passant! helper method checks whether:
    
    - the current move is diagonal (indicating a capture),
    - the move is to the piece's sixth rank (the enemy's third rank, which
      is always the row passed over by a two-square pawn advance), and
    - the game's last move was a two-square pawn advance on the same column
      as the current move's destination (which would have caused the game's
      en_passant_file attribute to be set to that column number).
    
    If all of those apply, an en_passant capture is occurring. The captured
    piece will have the same x_coord as the current move's destination, and
    the same y_coord that the capturing pawn had *before* the move. The
    piece on that square is then destroyed.

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -15,6 +15,23 @@ class Pawn < Piece
     false
   end
 
+  # This is an addition to the move_to! method in the Piece model.
+  # It destroys a pawn that has been captured en passant, determines whether
+  # the current pawn is vulnerable to en passant capture, and adds a
+  # placeholder for pawn promotion.
+  def move_to!(destination_x, destination_y)
+    capture_en_passant!(destination_x, destination_y)
+
+    # If the pawn is advancing two squares, it my be captured en passant.
+    en_passant_file = (destination_y - y_coord).abs == 2 ? x_coord : nil
+    game.update_attribute(:en_passant_file, en_passant_file)
+
+    super
+
+    # TODO: Implement the promote! method.
+    # promote! if y_coord == nth_rank(8)
+  end
+
   private
 
   def valid_standard_move?(new_x, new_y)
@@ -42,5 +59,20 @@ class Pawn < Piece
     # two squares on the destination file. If this is the case,
     # game.en_passant_file will equal the destination file.
     game.en_passant_file == new_x
+  end
+
+  def capture_en_passant!(destination_x, destination_y)
+    # If the pawn's x value is changing, the move must be a capture.
+    # If the destination rank is the current player's sixth rank (the enemy's
+    # third), and the last move was a two-square advance by a pawn on the
+    # destination file, it must be en passant. In that case, capture the enemy
+    # pawn, which is currently on the starting rank of the moving pawn.
+    if destination_x != x_coord &&
+       destination_y == nth_rank(6) &&
+       game.reload.en_passant_file == destination_x
+
+      # raise "#{game.reload.en_passant_file} == #{destination_x}"
+      game.piece_at(destination_x, y_coord).destroy
+    end
   end
 end
