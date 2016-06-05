aff7637e7ca3782043dde8e471982f1aff316430
bennypaulino <santiagopro71@gmail.com>
2016-06-05 15:05:42 -0400

    Add a moved column to the pieces table.
    This will indicate, either true or false, whether a piece has been moved or not

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160522191002) do
+ActiveRecord::Schema.define(version: 20160605185936) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20160522191002) do
     t.integer "x_coord"
     t.integer "y_coord"
     t.string  "img"
+    t.boolean "moved",      default: false
   end
 
   add_index "pieces", ["game_id"], name: "index_pieces_on_game_id", using: :btree
