f090c308a646768a46a9005c74dfe8a81a678d87
bennypaulino <santiagopro71@gmail.com>
2016-06-05 16:57:53 -0400

    Ensure that castling only occurs on the same rank
    Limit castling to only the valid files (x_coords) relevant to castling.

--- app/models/king.rb
+++ app/models/king.rb
@@ -1,6 +1,5 @@
 class King < Piece
-  # This method does not yet consider castling, and does not
-  # disallow a king from moving into check.
+  # This method does not disallow a king from moving into check.
   def valid_move?(new_x, new_y)
     return false if off_board?(new_x, new_y)
     return false if current_square?(new_x, new_y)
@@ -12,11 +11,21 @@ class King < Piece
   end
 
   def valid_castling?(new_x, new_y)
-    rook_file = 1 if new_x == 3
-    rook_file = 8 if new_x == 7
-    return false if obstructed?(rook_file, new_y)
     return false if moved?
+    return false unless nth_rank(1) == new_y
+    return false unless new_x == 3 || new_x == 7
+    rook_file(new_x, new_y)
+  end
+
+  def nth_rank(n)
+    color == 'white' ? n : 9 - n
+  end
+
+  def rook_file(new_x, new_y)
+    rook_file = new_x == 3 ? 1 : 8
     rook = game.piece_at(rook_file, new_y)
     return false if rook.moved?
+    return false if obstructed?(rook_file, new_y)
+    true
   end
 end
