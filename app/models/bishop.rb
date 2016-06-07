1860b9d28c05cedcbe80a84cd756256eb089cbb3
Prateek Vasireddy <prat.vasireddy@gmail.com>
2016-06-07 12:04:04 +0000

    removed explicit returns from the move validation

--- app/models/bishop.rb
+++ app/models/bishop.rb
@@ -4,9 +4,8 @@ class Bishop < Piece
 
     return false if off_board?(new_x, new_y)
     return false if current_square?(new_x, new_y)
-    return true if diagonal_move?(new_x, new_y)
+    diagonal_move?(new_x, new_y)
 
-    false
   end
 
 end
