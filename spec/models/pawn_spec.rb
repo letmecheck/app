952c8879e0772025f51a6406e79feddf522b221e
Steve McClellan <steve470@gmail.com>
2016-06-12 15:44:50 -0400

    Add non-pawn and no-two-square-advance tests
    Verify that en passant capture cannot be made by a non-pawn, by
    attempting to take a pawn that just advanced two squares by moving
    a bishop to the square it passed through. This is a legal bishop move,
    but leaves the pawn uncaptured.
    
    Verify that en passant capture cannot be made on a pawn that has made
    two separate one-square advances, rather than a single two-square
    advance.

--- spec/models/pawn_spec.rb
+++ spec/models/pawn_spec.rb
@@ -4,35 +4,70 @@ RSpec.describe Pawn, type: :model do
   context "when set up for en passant" do
     let(:game) { Game.create! }
     let(:white_pawn_2) { game.piece_at(2, 2) }
-    let(:black_pawn_3) { game.piece_at(3, 7) }
+    let(:black_pawn_1) { game.piece_at(1, 7) }
     let(:white_pawn_5) { game.piece_at(5, 2) }
     let(:black_pawn_6) { game.piece_at(6, 7) }
     let(:white_knight) { game.piece_at(7, 1) }
     let(:black_knight) { game.piece_at(7, 8) }
+    let(:black_pawn_3) { game.piece_at(3, 7) }
+    let(:white_bishop) { game.piece_at(6, 1) }
 
     it "allows valid en passant capture" do
       white_pawn_2.move_to!(2, 4)
+      # Inconsequential move to bypass Black's turn:
       black_knight.move_to!(8, 6)
       white_pawn_2.move_to!(2, 5)
-      black_pawn_3.move_to!(3, 5)
-      expect(white_pawn_2.valid_move?(3, 6)).to be true
-      white_pawn_2.move_to!(3, 6)
-      expect { black_pawn_3.reload }.to raise_error(
+      # Two-square advance:
+      black_pawn_1.move_to!(1, 5)
+      # Capture:
+      expect(white_pawn_2.valid_move?(1, 6)).to be true
+      white_pawn_2.move_to!(1, 6)
+      expect { black_pawn_1.reload }.to raise_error(
         ActiveRecord::RecordNotFound
       )
     end
 
     it "disallows e.p. capture after another move has been made" do
-      black_pawn_6.move_to!(6, 5)
+      # Inconsequential move to bypass White's turn:
       white_knight.move_to!(8, 3)
+      black_pawn_6.move_to!(6, 5)
+      # Ditto:
+      white_knight.move_to!(7, 1)
       black_pawn_6.move_to!(6, 4)
+      # Two-square advance:
       white_pawn_5.move_to!(5, 4)
+      # Completely-unrelated moves by each player:
       black_knight.move_to!(8, 6)
-      white_knight.move_to!(7, 5)
+      white_knight.move_to!(8, 3)
+      # Invalid capture:
       expect(black_pawn_6.valid_move?(5, 3)).to be false
-      # This will work properly when valid_move? is required by move_to!:
-      # expect(black_pawn_6.move_to!(5, 3)).to be false
+      expect(black_pawn_6.move_to!(5, 3)).to be false
       expect(white_pawn_5.reload.id).to be_a Integer
+    end
+
+    it "disallows e.p. capture by a non-pawn" do
+      white_pawn_5.move_to!(5, 4)
+      # Inconsequential move to bypass Black's turn:
+      black_knight.move_to!(8, 6)
+      white_bishop.move_to!(2, 5)
+      # Two-square advance:
+      black_pawn_3.move_to!(3, 5)
+      # Invalid capture:
+      white_bishop.move_to!(3, 6)
+      expect(black_pawn_3.reload.id).to be_a Integer
+    end
+
+    it "disallows e.p. capture without a two-square advance" do
+      white_pawn_2.move_to!(2, 4)
+      # One-square advance:
+      black_pawn_1.move_to!(1, 6)
+      white_pawn_2.move_to!(2, 5)
+      # A second one-square advance:
+      black_pawn_1.move_to!(1, 5)
+      # Invalid capture:
+      expect(white_pawn_2.valid_move?(1, 6)).to be false
+      expect(white_pawn_2.move_to!(1, 6)).to be false
+      expect(black_pawn_1.reload.id).to be_a Integer
     end
   end
 end
