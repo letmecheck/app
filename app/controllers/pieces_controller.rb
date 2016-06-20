c7e69d46e6e5bc31457b8b86e2f136807aa7dd64
Steve McClellan <steve470@gmail.com>
2016-06-19 20:51:10 -0400

    Make update work even after invalid move

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -5,9 +5,10 @@ class PiecesController < ApplicationController
 
   def update
     @piece = Piece.find(params[:id])
+    original_x = @piece.x_coord
+    original_y = @piece.y_coord
     if @piece.move_to!(piece_params[:x_coord].to_i, piece_params[:y_coord].to_i)
-      # Trigger update notification.
-      Pusher["game-#{@piece.game.id}"].trigger('move_made', {})
+      trigger_notification(original_x, original_y)
     end
   end
 
@@ -15,5 +16,16 @@ class PiecesController < ApplicationController
 
   def piece_params
     params.require(:piece).permit(:x_coord, :y_coord)
+  end
+
+  def trigger_notification(original_x, original_y)
+    Pusher["game-#{@piece.game.id}"].trigger(
+      'move_made',
+      piece_url: piece_url(@piece),
+      orig_x: original_x,
+      orig_y: original_y,
+      dest_x: piece_params[:x_coord].to_i,
+      dest_y: piece_params[:y_coord].to_i
+    )
   end
 end
