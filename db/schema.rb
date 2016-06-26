774d3471463a91308cb599d5b183aa11c15b8be5
Prateek Vasireddy <prat.vasireddy@gmail.com>
2016-06-26 10:39:24 -0400

    Revert "Mailer"

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160625224244) do
+ActiveRecord::Schema.define(version: 20160622212427) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,7 +56,6 @@ ActiveRecord::Schema.define(version: 20160625224244) do
     t.datetime "updated_at",                                      null: false
     t.integer  "wins"
     t.integer  "losses"
-    t.datetime "last_request_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
