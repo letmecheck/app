fc5ecfba335a752814a658bef101270e247fc4bb
Farruh <ufarruh@gmail.com>
2016-06-27 09:43:05 -0700

    User can sign-in with username

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,16 +8,13 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-
-         def self.find_for_database_authentication(warden_conditions)
-               conditions = warden_conditions.dup
-               if login = conditions.delete(:login)
-                 where(conditions.to_hash).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
-               elsif conditions.has_key?(:username) || conditions.has_key?(:email)
-                 where(conditions.to_hash).first
-               end
-             end
-
-
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if (login = conditions.delete(:login))
+      where(conditions.to_hash).find_by(["lower(username) = :value OR lower(email) = :value", { value: login.downcase }])
+    elsif conditions.key?(:username) || conditions.key?(:email)
+      find_by(conditions.to_hash)
+    end
+  end
   has_many :games
 end
